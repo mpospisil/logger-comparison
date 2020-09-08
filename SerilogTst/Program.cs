fdea4c029fdc1c8e60980452276c816ef8007f95
Martin Pospisil <martin.pospisil@ideastatica.com>
2020-09-08 12:35:44 +0200

    adding Serilog test

---
+++ SerilogTst/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace SerilogTst
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		private static async Task MainAsync()
+		{
+			Console.WriteLine("Serilog performance test");
+
+			LogComparisonCommon.ILogger mainLogger = new SerilogFacade(typeof(Program));
+
+			//LogComparisonCommon.ILogger workLoadLogger = new SerilogFacade(typeof(LogComparisonCommon.WorkLoad));
+			var workLoad = new LogComparisonCommon.WorkLoad(mainLogger);
+
+			TimeSpan oneTaskTotalTime;
+
+			{
+				var msg = "Starting no log workload - synchronous";
+				Console.WriteLine(msg);
+				mainLogger.LogMessage(msg);
+				oneTaskTotalTime = await workLoad.DoOneTaskAsync(false);
+				msg = $"Serilog performance test is finished - time : {oneTaskTotalTime.TotalSeconds} s ({oneTaskTotalTime.TotalMilliseconds} ms)";
+
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+			}
+
+			{
+				var msg = "Starting log workload - synchronous";
+				Console.WriteLine(msg);
+				mainLogger.LogMessage(msg);
+				oneTaskTotalTime = await workLoad.DoOneTaskAsync(true);
+				msg = $"Serilog performance test is finished - time : {oneTaskTotalTime.TotalSeconds} s ({oneTaskTotalTime.TotalMilliseconds} ms)";
+
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+			}
+
+			{
+				int processorCount = Environment.ProcessorCount;
+				var msg = $"Starting asynchronous NLog performance test for {processorCount} running in parallel";
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+
+				oneTaskTotalTime = await workLoad.DoManyTasksAsync(processorCount, false);
+				msg = $"NLog performance test is finished - time : {oneTaskTotalTime.TotalSeconds} s ({oneTaskTotalTime.TotalMilliseconds} ms)";
+
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+			}
+
+			{
+				int processorCount = Environment.ProcessorCount;
+				var msg = $"Starting asynchronous NLog performance test for {processorCount} running in parallel";
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+
+				oneTaskTotalTime = await workLoad.DoManyTasksAsync(processorCount, true);
+				msg = $"NLog performance test is finished - time : {oneTaskTotalTime.TotalSeconds} s ({oneTaskTotalTime.TotalMilliseconds} ms)";
+
+				mainLogger.LogMessage(msg);
+				Console.WriteLine(msg);
+			}
+		}
+	}
+}
---
+++ SerilogTst/SerilogFacade.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace SerilogTst
+{
+	internal class SerilogFacade : LogComparisonCommon.ILogger
+	{
+		private readonly Serilog.Core.Logger Logger;
+
+		public SerilogFacade(Type type)
+		{
+			Logger = new LoggerConfiguration()
+					//.WriteTo.Console()
+					.WriteTo.File("c:\\temp\\serilog-comparison",
+				outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+					.Enrich.WithThreadId()
+					.CreateLogger();
+
+		}
+
+		public void LogMessage(string msg)
+		{
+			Logger.Information(msg);
+		}
+	}
+}
